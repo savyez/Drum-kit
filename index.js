7828263d24421d8b829d32876f0186e268e9f6e7
Savyez <visnuk252@gmail.com>
2026-01-13 16:18:46 +0530

    Added eventListener for Buttons

---
+++ index.js
@@ -0,0 +1,9 @@
+// alert("Hello");
+
+for (var i=0; i< document.querySelectorAll(".drum").length; i++ ) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        alert("I got clicked!");
+    });
+    
+};
