2b8419e652d58e25e6e32f70d0cf32c1c317c2e5
Savyez <visnuk252@gmail.com>
2026-01-13 21:28:05 +0530

    added sound and background images

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 for (var i=0; i< document.querySelectorAll(".drum").length; i++ ) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("I got clicked!");
+        new Audio("sounds/tom-1.mp3").play();
     });
-    
+
 };
