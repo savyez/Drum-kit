33c911b929f2abf06a8d3956927f4d2dbc32aee7
Savyez <visnuk252@gmail.com>
2026-01-13 22:37:59 +0530

    Function added for input

--- index.js
+++ index.js
@@ -3,39 +3,8 @@
 for (var i=0; i< document.querySelectorAll(".drum").length; i++ ) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
-        switch (this.innerHTML) {
-            case "w":
-                var tom1 = new Audio("sounds/tom-1.mp3");
-                tom1.play();
-                break;
-            case "a":
-                var tom2 = new Audio("sounds/tom-2.mp3");
-                tom2.play();
-                break;
-            case "s":
-                var tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play();
-                break;
-            case "d":
-                var tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play();
-                break;
-            case "j":
-                var crash = new Audio("sounds/crash.mp3");
-                crash.play();
-                break;
-            case "k":
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play();
-                break;
-            case "l":
-                var kick = new Audio("sounds/kick-bass.mp3");
-                kick.play();
-                break;
-            default:
-                console.log(this.innerHTML);
-        }
+        var buttonInnerHTML = this.innerHTML;
+        instrumentSound(buttonInnerHTML);
     });
 
 };
@@ -43,8 +12,15 @@ for (var i=0; i< document.querySelectorAll(".drum").length; i++ ) {
 
 // Detecting Keyboard Press
 
-document.addEventListener("keydown", function(event) {
-    switch (event.key) {
+document.addEventListener("keydown", function(event) {    
+    instrumentSound(event.key);
+});
+
+
+// Function to play sound based on key
+
+function instrumentSound(key) {
+    switch (key) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
@@ -73,5 +49,7 @@ document.addEventListener("keydown", function(event) {
             var kick = new Audio("sounds/kick-bass.mp3");
             kick.play();
             break;
-    }
-});
+        default:
+            console.log(key);
+        }
+}
